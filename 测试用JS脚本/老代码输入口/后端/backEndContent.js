05c8a3d7fba6d1baa7b724c9a7e21acb59d20db2
6008 <2066687453@qq.com>
2022-10-19 19:52:16 +0800

    后端公用代码生成

--- 测试用JS脚本/老代码输入口/后端/backEndContent.js
+++ 测试用JS脚本/老代码输入口/后端/backEndContent.js
@@ -1,4 +1,52 @@
 //controller
-var controller = ''
+//假设
+var inputNameChinese = ''
+var inputNameEnglish = ''
+function controllerContent(fileName,allControllerReflect,searchControllerReflect,
+                           AllcommentChinese,MethodscommentChinese){
+    var controller = '@RestController\n' +
+        '@RequestMapping("/'+allControllerReflect+'")\n' +
+        '@Api(value = "'+AllcommentChinese+'",tags = "'+AllcommentChinese+'")\n' +
+        'public class '+fileName+'Controller'+' {\n' +
+        '\n' +
+        '    @Autowired\n' +
+        '    private '+fileName+'Service'+' '+allControllerReflect+'Service'+';\n' +
+        '\n' +
+        '    @PostMapping("/'+searchControllerReflect+'")\n' +
+        '    @ApiOperation(value = "'+MethodscommentChinese+'",notes = "'+MethodscommentChinese+'")\n' +
+        '    public List<Map<String, Object>> '+searchControllerReflect+'(@RequestBody JSONObject param) throws Exception {\n' +
+        '        return '+allControllerReflect+'Service'+'.'+searchControllerReflect+'(param);\n' +
+        '    }\n' +
+        '\n' +
+        '}\n'
+    return controller
+}
+
 //service
+function serviceContent(fileName,allControllerReflect,searchControllerReflect,
+                        AllcommentChinese,MethodscommentChinese){
+    var service = 'public interface '+fileName+'Service'+' extends BaseService {\n' +
+        '    List<Map<String, Object>> '+searchControllerReflect+'(JSONObject param) throws Exception;\n' +
+        '\n' +
+        '}'
+    return service
+}
 //serviceImpl
+function serviceImplContent(fileName,allControllerReflect,searchControllerReflect,
+                            AllcommentChinese,MethodscommentChinese) {
+    var serviceImpl = '@Service\n' +
+        '@Slf4j\n' +
+        'public class '+fileName+'ServiceImpl'+' extends BaseServiceImpl implements '+fileName+'Service'+' {\n' +
+        '    @Autowired\n' +
+        '    private JdbcTemplate jdbcTemplate;\n' +
+        '\n' +
+        '\n' +
+        '    //'+MethodscommentChinese+'\n' +
+        '    @Override\n' +
+        '    public List<Map<String, Object>> '+searchControllerReflect+'(JSONObject param) throws Exception{\n' +
+        '        return null;\n' +
+        '    }\n' +
+        '\n' +
+        '}\n'
+    return serviceImpl
+}
