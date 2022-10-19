66dd4dc1a12dca962dd410b76b7007ebd8da38bf
6008 <2066687453@qq.com>
2022-10-19 20:07:40 +0800

    后端公用代码生成

--- 测试用JS脚本/老代码输入口/后端/backEndContent.js
+++ 测试用JS脚本/老代码输入口/后端/backEndContent.js
@@ -1,10 +1,19 @@
 //controller
 //假设
-var inputNameChinese = ''
-var inputNameEnglish = ''
 function controllerContent(fileName,allControllerReflect,searchControllerReflect,
                            AllcommentChinese,MethodscommentChinese){
-    var controller = '@RestController\n' +
+    var controller = 'import com.alibaba.fastjson.JSONObject;\n' +
+        'import io.swagger.annotations.Api;\n' +
+        'import io.swagger.annotations.ApiOperation;\n' +
+        'import org.springframework.beans.factory.annotation.Autowired;\n' +
+        'import org.springframework.web.bind.annotation.PostMapping;\n' +
+        'import org.springframework.web.bind.annotation.RequestBody;\n' +
+        'import org.springframework.web.bind.annotation.RequestMapping;\n' +
+        'import org.springframework.web.bind.annotation.RestController;\n' +
+        '\n' +
+        'import java.util.List;\n' +
+        'import java.util.Map;'+'\n\n\n\n\n'+
+        '@RestController\n' +
         '@RequestMapping("/'+allControllerReflect+'")\n' +
         '@Api(value = "'+AllcommentChinese+'",tags = "'+AllcommentChinese+'")\n' +
         'public class '+fileName+'Controller'+' {\n' +
@@ -25,7 +34,11 @@ function controllerContent(fileName,allControllerReflect,searchControllerReflect
 //service
 function serviceContent(fileName,allControllerReflect,searchControllerReflect,
                         AllcommentChinese,MethodscommentChinese){
-    var service = 'public interface '+fileName+'Service'+' extends BaseService {\n' +
+    var service = 'import com.alibaba.fastjson.JSONObject;\n' +
+        '\n' +
+        'import java.util.List;\n' +
+        'import java.util.Map;'+'\n\n\n\n\n'+
+        'public interface '+fileName+'Service'+' extends BaseService {\n' +
         '    List<Map<String, Object>> '+searchControllerReflect+'(JSONObject param) throws Exception;\n' +
         '\n' +
         '}'
@@ -34,7 +47,16 @@ function serviceContent(fileName,allControllerReflect,searchControllerReflect,
 //serviceImpl
 function serviceImplContent(fileName,allControllerReflect,searchControllerReflect,
                             AllcommentChinese,MethodscommentChinese) {
-    var serviceImpl = '@Service\n' +
+    var serviceImpl = 'import com.alibaba.fastjson.JSONObject;\n' +
+        'import com.dareway.serviceDwdsmDeviceAccess.service.base.impl.BaseServiceImpl;\n' +
+        'import lombok.extern.slf4j.Slf4j;\n' +
+        'import org.springframework.beans.factory.annotation.Autowired;\n' +
+        'import org.springframework.jdbc.core.JdbcTemplate;\n' +
+        'import org.springframework.stereotype.Service;\n' +
+        '\n' +
+        'import java.util.List;\n' +
+        'import java.util.Map;'+'\n\n\n\n\n\n'+
+        '@Service\n' +
         '@Slf4j\n' +
         'public class '+fileName+'ServiceImpl'+' extends BaseServiceImpl implements '+fileName+'Service'+' {\n' +
         '    @Autowired\n' +
